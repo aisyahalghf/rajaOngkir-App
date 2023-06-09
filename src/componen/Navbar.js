59f4445fec149503a03d961ec9e38ca37b39af86
aisyahalghf <alghifariaisyahputri@gmail.com>
2023-06-09 09:12:25 +0700

    name-app

--- src/componen/Navbar.js
+++ src/componen/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="  shadow shadow-slate-200 sticky top-0 z-10 bg-white px-2 md:px-0 ">
       <div className=" flex justify-start text-stone-500 h-20 items-center container mx-auto  ">
-        <h1 className=" text-xl font-bold font-shadow ">SendSages</h1>
+        <h1 className=" text-xl font-bold font-shadow ">SendSage</h1>
       </div>
     </nav>
   );
